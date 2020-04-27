a32ef09c8ac9eda465fb62963bc6508186f2c2b5
stefantrinh1 <Zaya@Zaya-MacAir.hsd1.ca.comcast.net>
2020-04-26 23:04:52 -0700

    adding reflection questions

--- src/pages/reflections.js
+++ src/pages/reflections.js
@@ -6,8 +6,30 @@ import UnderConstruction from "../components/UnderConstruction";
 export default ({ data }) => (
 <Layout>
 <SEO title="Reflections" description="an interview with Zaya Jones and her current medical school reflections" />
+
+<div style={{padding:"1em"}}>
 <h1 style={{textAlign:"center"}}>Reflections</h1>
-  <UnderConstruction />
+  
+<h3>1.	Which 3 words best describe your absolute best self? Would you say others would
+describe you using those words now? What could you do to challenge yourself to
+develop those characteristics.</h3>
+
+<p>The three words that describe my absolute best self would be kind, understanding, and persistent. My husband would say determined, persistent, and analytical. Others around me, I hope would say similar kind words about me. Developing these characteristics takes time and does not come from one challenge, but more life experiences. I would say I learned understanding and kindness to others mostly because I grew up as an outsider in most situations. Walking even a foot in someone else’s shoes can teach you a lot about kindness and understanding. Because of my ambitious nature, I often strive for things that are not easily achievable which has caused failures in my life. Striving to achieve my ambitions has made me realize that anything can be accomplished if you continue to persevere. </p>
+
+<h3>From the list you chose above, how do those words apply to the type of physician you
+want to be?</h3>
+
+<p>I would hope that kindness and understanding is a descriptor that most physicians and soon to be physicians describe themselves as. I strive to be a physician that tries to understand patients without my own biases. </p>
+
+<h3>What activities do you enjoy most that you seem to get lost in them?</h3>
+
+<p>In essence, any activity where my hands are busy and I can focus on what I am doing such as reading, cooking, and knitting. I enjoy reading because I can get lost in various different worlds that I would never be able to experience in real life. Cooking is something I enjoy because I can create delicious meals for people to enjoy. I often lose track of time when I am knitting because of the rhythmic movements. </p>
+
+<br />
+
+
+</div>
+
 </Layout>
 
 )
